33e46a933a673b10ef49dad63ddcc2b26b475032
Paul Metzger <s1576303@sms.ed.ac.uk>
2021-09-01 09:19:14 +0100

    Fixed index computations.

--- apps/StencilFast/StencilFast.cpp
+++ apps/StencilFast/StencilFast.cpp
@@ -12,7 +12,7 @@
 
 void populate_in_buf(int *in_buf, int x_size, int y_size) {
   for (int y = 0; y < y_size; ++y) {
-    for (int x = 0; x < x_size; ++x) in_buf[y * y_size + x] = x * y;
+    for (int x = 0; x < x_size; ++x) in_buf[y * x_size + x] = x * y;
   }
 }
 
@@ -21,13 +21,13 @@ void populate_in_buf(int *in_buf, int x_size, int y_size) {
 void generate_golden_output(int *in_buf, int *golden_out, int x_size, int y_size) {
   for (int y = 0; y < y_size; ++y) {
     for (int x = 0; x < x_size; ++x) {
-      const int ind = y * y_size + x;
+      const int ind = y * x_size + x;
 
       int result = in_buf[ind];
-      if (x < x_size - 1) result += in_buf[y * y_size + x + 1];
-      if (x > 0)          result += in_buf[y * y_size + x - 1];
-      if (y < y_size - 1) result += in_buf[(y + 1) * y_size + x];
-      if (y > 0)          result += in_buf[(y - 1) * y_size + x];
+      if (x < x_size - 1) result += in_buf[y * x_size + x + 1];
+      if (x > 0)          result += in_buf[y * x_size + x - 1];
+      if (y < y_size - 1) result += in_buf[(y + 1) * x_size + x];
+      if (y > 0)          result += in_buf[(y - 1) * x_size + x];
       golden_out[ind] = result;
     }
   }
@@ -55,7 +55,7 @@ struct SimpleStencil : Kernel {
   void kernel() {
     const int x = blockIdx.x * blockDim.x + threadIdx.x;
     const int y = blockIdx.y * blockDim.y + threadIdx.y;
-    const int ind = y * y_size + x;
+    const int ind = y * x_size + x;
 
     // Load values into local memory
     auto c = shared.array<int, SIMTLanes, SIMTWarps>();
@@ -65,25 +65,25 @@ struct SimpleStencil : Kernel {
     int result = c[threadIdx.y][threadIdx.x];
     if (x < x_size - 1) {
       if (threadIdx.x == blockDim.x - 1) result += in_buf[ind + 1];
-      else result += c[threadIdx.y][threadIdx.x + 1]; // in_buf[y * y_size + x + 1];
+      else result += c[threadIdx.y][threadIdx.x + 1]; // in_buf[y * x_size + x + 1];
     }
     noclConverge();
 
     if (x > 0) {
       if (threadIdx.x == 0) result += in_buf[ind - 1];
-      else result += c[threadIdx.y][threadIdx.x - 1]; //in_buf[y * y_size + x - 1];
+      else result += c[threadIdx.y][threadIdx.x - 1]; //in_buf[y * x_size + x - 1];
     }
     noclConverge();
 
     if (y < y_size - 1) {
-      if (threadIdx.y == blockDim.y - 1) result += in_buf[(y + 1) * y_size + x];
-      else result += c[threadIdx.y + 1][threadIdx.x]; // in_buf[(y + 1) * y_size + x];
+      if (threadIdx.y == blockDim.y - 1) result += in_buf[(y + 1) * x_size + x];
+      else result += c[threadIdx.y + 1][threadIdx.x]; // in_buf[(y + 1) * x_size + x];
     }
     noclConverge();
 
     if (y > 0) {
-      if (threadIdx.y == 0) result += in_buf[(y - 1) * y_size + x];
-      else result += c[threadIdx.y - 1][threadIdx.x]; // in_buf[(y - 1) * y_size + x];
+      if (threadIdx.y == 0) result += in_buf[(y - 1) * x_size + x];
+      else result += c[threadIdx.y - 1][threadIdx.x]; // in_buf[(y - 1) * x_size + x];
     }
     noclConverge();
 
